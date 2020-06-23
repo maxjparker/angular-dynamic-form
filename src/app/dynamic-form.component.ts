63c4476b07c4c99bbad21667b387efa9cab2a0f5
Maximilian <maxim.j.parker@gmail.com>
2020-06-23 16:13:18 -0600

    added component that takes in dynamic questions and produces dynamic form

---
+++ src/app/dynamic-form.component.ts
@@ -0,0 +1,44 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+
+import {QuestionBase} from './question-base';
+import {QuestionControlService} from './question-control.service';
+
+@Component({
+  selector: 'app-dynamic-form',
+  template: `
+    <div>
+      <form (ngSubmit)="onSubmit()" [formGroup]="form">
+        <div *ngFor="let question of questions" class="form-row">
+          <app-question [question]="question" [form]="form"></app-question>
+        </div>
+
+        <div class="form-row">
+          <button type="submit" [disabled]="!form.valid">Save</button>
+        </div>
+      </form>
+
+      <div *ngIf="payLoad" class="form-row">
+        <strong>Saved the following values</strong><br>{{payLoad}}
+      </div>
+    </div>
+  `,
+  providers: [QuestionControlService]
+})
+export class DynamicFormComponent implements OnInit {
+
+  @Input() questions: QuestionBase<string>[] = [];
+  form: FormGroup;
+  payLoad = '';
+
+  constructor(private qcs: QuestionControlService) {}
+
+  ngOnInit() {
+    this.form = this.qcs.toFormGroup(this.questions);
+  }
+
+  onSubmit() {
+    this.payLoad = JSON.stringify(this.form.getRawValue());
+  }
+
+}
